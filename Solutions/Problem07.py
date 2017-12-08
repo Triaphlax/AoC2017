2af8e792133f3675802c5b77b52147a75c81eb20
Rolf Plagmeijer <sausage948@gmail.com>
2017-12-08 01:11:57 +0100

    Solved Day 7

--- Solutions/Problem07.py
+++ Solutions/Problem07.py
@@ -15,32 +15,57 @@ while True:
 
 
 def parseString(string):
-    splitUp = re.split(" \(|\) -> |\)|, ", string)
+    splitUp = re.split(' \(|\) -> |\)|, ', string)
     childrensList = []
     for n in splitUp[2:]:
         if n != '':
             childrensList.append(n)
-    return splitUp[0], splitUp[1], childrensList
+
+    weights = []
+    legacy = []
+    if not childrensList:
+        weights = [0, 0, 0]
+        legacy = [0, 0, 0]
+
+    return splitUp[0], int(splitUp[1]), childrensList, weights, legacy
 
 
-betterInputList = list(map(parseString, inputList))
+bestList = list(map(parseString, inputList))
 
-# ------Part 1------ #
-part1List = list(betterInputList)
-while len(part1List) > 1:
-    for i, entry in enumerate(part1List):
+
+# ------Parts 1 and 2------ #
+def part2Answer(megaTuple):
+    sortedWeights = sorted(megaTuple[3])
+    numberOfWeights = len(sortedWeights)
+    if sortedWeights[0] < sortedWeights[1]:
+        difference = sortedWeights[1] - sortedWeights[0]
+        wrongValue = sortedWeights[0]
+    else:
+        difference = sortedWeights[numberOfWeights-2] - sortedWeights[numberOfWeights-1]
+        wrongValue = sortedWeights[numberOfWeights-1]
+    return megaTuple[4][megaTuple[3].index(wrongValue)] + difference
+
+
+while len(bestList) > 1:
+    for i, entry in enumerate(bestList):
         if not entry[2]:
-            thirds = [i[2] for i in part1List]
+            thirds = [i[2] for i in bestList]
             for j, l in enumerate(thirds):
                 if entry[0] in l:
-                    part1List[j][2].remove(entry[0])
-                    del part1List[i]
+                    bestList[j][2].remove(entry[0])
+                    bestList[j][3].append(entry[1] + sum(entry[3]))
+                    bestList[j][4].append(entry[1])
+                    del bestList[i]
+                    if len(set(entry[3])) > 1:
+                        if answer2 == 0:
+                            answer2 = part2Answer(entry)
                     break
             break
 
-answer1 = part1List[0][0]
+answer1 = bestList[0][0]
 
-# ------Part 2------ #
+if answer2 == 0:
+    answer2 = part2Answer(bestList[0])
 
 # ------Output----- #
 print("Answer 1: " + str(answer1))
